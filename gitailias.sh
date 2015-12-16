a27cb09ff90f8a4a21e7c2bbabc5c1235e6388cb
lihongjun <lihongjun@aizhetech.com>
2015-12-16 19:07:51 +0800

    add command

---
+++ gitailias.sh
@@ -0,0 +1,6 @@
+#! /sbin/sh
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.last 'log -3'
+
+
